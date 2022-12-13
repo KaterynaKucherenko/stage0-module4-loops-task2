a5765c6c4874dd0feec5140920e0ccb9b9de1bb4
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-13 17:40:59 +0300

    fix

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -4,6 +4,7 @@ public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
                 int x = 1;
 		int y = 1;
+		System.out.println(1);
 		while (y<=printToInclusive && y>0){
 		    x*=y;
 		    System.out.println(x);
